b36f3a3d5d04a6d23745b14346e7e69b3eed6f5a
Rony <rony.dormevil@gmail.com>
2021-03-16 10:27:01 +0100

    add project

---
+++ src/main/java/com/project/manhwa/ManhwaApplication.java
@@ -0,0 +1,13 @@
+package com.project.manhwa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManhwaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ManhwaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/manhwa/ManhwaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.manhwa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ManhwaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
